7dea80ed3d4e828bc72981fc114ac6485c54ef2b
Irina Nicolai <irina.nic.yu@gmail.com>
2022-04-07 17:55:27 +0200

    Add layout for main section header

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,7 +3,31 @@ import styles from './Main.module.scss';
 
 const Main = () => {
     return (
-        <div>THis is Main</div>
+        <main className={styles.main}>
+            <div className={styles.main__header}>
+                <h2 className={styles.main__title}>Glasses</h2>
+                <form className={styles.main__form}>
+                    <h3 className={styles.main__formHeader}>Color:</h3>
+                    <div className={styles.main__formGroup}>
+                        <label className={styles.main__label}>
+                            <input type="radio" name="color" className={styles.main__input} />
+                        </label>
+                        <label className={styles.main__label}>
+                            <input type="radio" name="color" className={styles.main__input} />
+                        </label>
+                        <label className={styles.main__label}>
+                            <input type="radio" name="color" className={styles.main__input} />
+                        </label>
+                        <label className={styles.main__label}>
+                            <input type="radio" name="color" className={styles.main__input} />
+                        </label>
+                    </div>
+                </form>
+            </div>
+            <div className={styles.main__body}>
+
+            </div>
+        </main>
     )
 }
 
