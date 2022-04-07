f324f76a051d6e27c5938f7f21f135c2b1b2fef3
Irina Nicolai <irina.nic.yu@gmail.com>
2022-04-07 16:14:04 +0200

    Add main styling to app container

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Main from './components/Main/Main.js';
+import App from './components/App/App';
 import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<Main />);
+root.render(<App />);
